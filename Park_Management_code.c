b6fbe0bfdad2e91c661f2ac44c1d71e43b003168
ariansust21 <pillowtalkerarian@gmail.com>
2023-12-25 22:26:06 +0600

    Taking Inputs

--- Park_Management_code.c
+++ Park_Management_code.c
@@ -1,10 +1,17 @@
 #include<stdio.h>
 #include<windows.h>
+int date,month,year;
 
 int main(){
    system("CLS");
    system("color 0A");
 
-   
+   printf("\n Enter TODAYS DATE FOR CONTINUE ");
+   printf("\n\tENTER DATE: ");
+   scanf("%d", &date);
+   printf("\n\tENTER MONTH: ");
+   scanf("%d", &month);
+   printf("\n\tENTER YEAR: ");
+   scanf("%d", &year);
 
 }
